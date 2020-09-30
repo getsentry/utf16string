b5b07a1ef83352e7de003f0afcd0d79615f365c9
Floris Bruynooghe <flub@devork.be>
2020-09-30 21:30:25 +0200

    Move iter tests over to iter module

--- src/iters.rs
+++ src/iters.rs
@@ -116,3 +116,124 @@ where
 }
 
 impl<'a, E> FusedIterator for WStrCharIndices<'a, E> where E: ByteOrder {}
+
+#[cfg(test)]
+mod tests {
+    use crate::WStr;
+
+    #[test]
+    fn test_wstr_chars() {
+        let b = b"h\x00e\x00l\x00l\x00o\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let chars: Vec<char> = s.chars().collect();
+        assert_eq!(chars, vec!['h', 'e', 'l', 'l', 'o']);
+
+        let b = b"\x00\xd8\x00\xdcx\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let chars: Vec<char> = s.chars().collect();
+        assert_eq!(chars, vec!['\u{10000}', 'x']);
+
+        // Regression: this leading surrogate used to be badly detected.
+        let b = b"\x41\xf8A\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let chars: Vec<char> = s.chars().collect();
+        assert_eq!(chars, vec!['\u{f841}', 'A']);
+    }
+
+    #[test]
+    fn test_wstr_chars_reverse() {
+        let b = b"h\x00e\x00l\x00l\x00o\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let chars: Vec<char> = s.chars().rev().collect();
+        assert_eq!(chars, vec!['o', 'l', 'l', 'e', 'h']);
+
+        let b = b"\x00\xd8\x00\xdcx\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let chars: Vec<char> = s.chars().rev().collect();
+        assert_eq!(chars, vec!['x', '\u{10000}']);
+    }
+
+    #[test]
+    fn test_wstr_chars_last() {
+        let b = b"h\x00e\x00l\x00l\x00o\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let c = s.chars().last().unwrap();
+        assert_eq!(c, 'o');
+
+        let b = b"\x00\xd8\x00\xdcx\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let c = s.chars().last().unwrap();
+        assert_eq!(c, 'x');
+    }
+
+    #[test]
+    fn test_wstr_chars_count() {
+        let b = b"h\x00e\x00l\x00l\x00o\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let n = s.chars().count();
+        assert_eq!(n, 5);
+
+        let b = b"\x00\xd8\x00\xdcx\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let n = s.chars().count();
+        assert_eq!(n, 2);
+    }
+
+    #[test]
+    fn test_wstr_char_indices() {
+        let b = b"h\x00e\x00l\x00l\x00o\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let chars: Vec<(usize, char)> = s.char_indices().collect();
+        assert_eq!(
+            chars,
+            vec![(0, 'h'), (2, 'e'), (4, 'l'), (6, 'l'), (8, 'o')]
+        );
+
+        let b = b"\x00\xd8\x00\xdcx\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let chars: Vec<(usize, char)> = s.char_indices().collect();
+        assert_eq!(chars, vec![(0, '\u{10000}'), (4, 'x')]);
+    }
+
+    #[test]
+    fn test_wstr_char_indices_reverse() {
+        let b = b"h\x00e\x00l\x00l\x00o\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let chars: Vec<(usize, char)> = s.char_indices().rev().collect();
+        assert_eq!(
+            chars,
+            vec![(8, 'o'), (6, 'l'), (4, 'l'), (2, 'e'), (0, 'h')]
+        );
+
+        let b = b"\x00\xd8\x00\xdcx\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let chars: Vec<(usize, char)> = s.char_indices().rev().collect();
+        assert_eq!(chars, vec![(4, 'x'), (0, '\u{10000}')]);
+    }
+
+    #[test]
+    fn test_wstr_char_indices_last() {
+        let b = b"h\x00e\x00l\x00l\x00o\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let c = s.char_indices().last().unwrap();
+        assert_eq!(c, (8, 'o'));
+
+        let b = b"\x00\xd8\x00\xdcx\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let c = s.char_indices().last().unwrap();
+        assert_eq!(c, (4, 'x'));
+    }
+
+    #[test]
+    fn test_wstr_char_indices_count() {
+        let b = b"h\x00e\x00l\x00l\x00o\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let n = s.char_indices().count();
+        assert_eq!(n, 5);
+
+        let b = b"\x00\xd8\x00\xdcx\x00";
+        let s = WStr::from_utf16le(b).unwrap();
+        let n = s.char_indices().count();
+        assert_eq!(n, 2);
+    }
+}
